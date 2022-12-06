2d53736bf06446b2d8335bb90012e596f94317e6
okmtyuta <okmtyuta@gmail.com>
2022-12-06 19:57:41 +0900

    make push

--- packages/manage.py
+++ packages/manage.py
@@ -5,12 +5,20 @@ from app import App
 from urls import url_patterns
 
 
+def to_view(view_function):
+    def callback(request, start_response):
+        start_response("200 OK", [("Content-type", "text/plain; charset=utf-8")])
+        view_function(request)
+    
+    return callback
+
+
 def main(args):
     if args.action == "runserver":
         app = App()
 
         for url_pattern in url_patterns:
-            app.router.add(url_pattern[0], url_pattern[1], url_pattern[2])
+            app.router.add(url_pattern[0], url_pattern[1], to_view(url_pattern[2]))
 
         httpd = make_server("", 8000, app)
 
--- packages/views.py
+++ packages/views.py
@@ -1,9 +1,7 @@
 # ここにはview関数を実装のこと
 
-def index(request, start_response):
-    start_response("200 OK", [("Content-type", "text/plain; charset=utf-8")])
+def index(request):
     return [b"THIS PAGE IS INDEX"]
 
-def okmtyuta(request, start_response):
-    start_response("200 OK", [("Content-type", "text/plain; charset=utf-8")])
+def okmtyuta(request):
     return [b"Hello okmtyuta"]
