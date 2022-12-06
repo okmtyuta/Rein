712442b4008e561860d17107ce791f44e05748b9
okmtyuta <okmtyuta@gmail.com>
2022-12-06 22:12:06 +0900

    make

--- packages/setting.py
+++ packages/setting.py
@@ -1,3 +1,3 @@
-_TEMPLATE_DIRS = ["/Users/okamotoyouta/Desktop/Productions/Rein/packages/template"]
+_TEMPLATE_DIRS = ["", "/Users/okamotoyouta/Desktop/Productions/Rein/packages/template"]
 
 _DEBUG = True
