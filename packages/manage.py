4c1d5dc93757892916cf718150c539b2f5a55bcb
okmtyuta <okmtyuta@gmail.com>
2022-12-07 19:34:37 +0900

    make

--- packages/manage.py
+++ packages/manage.py
@@ -6,6 +6,7 @@ from urls import url_patterns
 
 
 def main(args):
+    # 開発用サーバー起動用
     if args.action == "runserver":
         app = App()
 
@@ -15,7 +16,6 @@ def main(args):
             )
 
         httpd = make_server("", int(args.port), app)
-
         print(f"Server started on http://localhost:{args.port}")
         httpd.serve_forever()
 
@@ -24,10 +24,10 @@ def main(args):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="")
-    parser.add_argument("--action", default="runserver", help="")
-    parser.add_argument("--port", default=8000, help="")
+    parser = argparse.ArgumentParser(description="Rein管理プログラム")
+    parser.add_argument("--action", default="runserver", help="manage.pyのアクション指定")
+    parser.add_argument("--port", default=8000, help="開発用サーバーを起動するためのポート指定")
 
     args = parser.parse_args()
 
-    app = main(args)
+    main(args)
